4d4607d15028154e2e3f8eba191f1a865a8bf7f1
oiranca <samuelromeroarbelo@gmail.com>
2024-07-20 13:27:01 +0100

    social component

---
+++ src/app/components/Social-Networks/SocialNetworks.tsx
@@ -0,0 +1,45 @@
+export const SocialNetworks = () => {
+  const linkedin = './img/social/linkedin_logo.png';
+  const x = '/img/social/X_logo.png';
+  const github = '/img/social/github_logo.png';
+  return (
+    <section className='grid grid-cols-3 justify-items-center content-around gap-x-6 gap-y-2 p-2 w-fit '>
+      <a
+        href='https://www.linkedin.com/in/samuel-romero-arbelo/'
+        className='group rounded-lg border border-transparent py-4 transition-colors'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img
+          className=' rounded-full w-8 h-8 md:w-12 md:h-12 hover:shadow-md hover:shadow-amber-50'
+          src={linkedin}
+          alt='Link a mi perfil de LinkedIn'
+        />
+      </a>
+      <a
+        href='https://x.com/Oiranca'
+        className='group rounded-lg border border-transparent py-4 transition-colors'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img
+          className=' rounded-full w-8 h-8 md:w-12 md:h-12 hover:shadow-md hover:shadow-amber-50'
+          src={x}
+          alt='Link a mi perfil de X anterior twitter'
+        />
+      </a>
+      <a
+        href='https://github.com/Oiranca'
+        className='group rounded-lg border border-transparent py-4 transition-colors'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img
+          className=' rounded-full w-8 h-8 md:w-12 md:h-12 hover:shadow-md hover:shadow-amber-50'
+          src={github}
+          alt='Link a mi perfil de GitHub'
+        />
+      </a>
+    </section>
+  );
+};
--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,18 +1,23 @@
+import { SocialNetworks } from '@/app/components/Social-Networks/SocialNetworks';
+
 export const Navbar = () => {
   return (
-    <ul className='w-full font-mono text-amber-50 grid grid-cols-2 grid-rows-2 justify-items-center content-around gap-x-4 gap-y-2 p-2 border border-gray-300 rounded-xl sm:text-xl md:text-2xl md:grid-cols-4 md:grid-rows-1 lg:grid-cols-4 lg:grid-rows-1 lg:text-3xl lg:w-fit'>
-      <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
-        <a href='#about'>Sobre mí</a>
-      </li>
-      <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
-        <a href='#experience'>Experiencia</a>
-      </li>
-      <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
-        <a href='#projects'>Proyectos</a>
-      </li>
-      <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
-        <a href='#contact'>Contacto</a>
-      </li>
-    </ul>
+    <nav className='flex flex-col items-center'>
+      <ul className='w-full font-mono text-amber-50 grid grid-cols-2 grid-rows-2 justify-items-center content-around gap-x-4 gap-y-2 p-2 border border-gray-300 rounded-xl sm:text-xl md:text-2xl md:grid-cols-4 md:grid-rows-1 lg:grid-cols-4 lg:grid-rows-1 lg:text-3xl lg:w-fit'>
+        <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
+          <a href='#about'>Sobre mí</a>
+        </li>
+        <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
+          <a href='#experience'>Experiencia</a>
+        </li>
+        <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
+          <a href='#projects'>Proyectos</a>
+        </li>
+        <li className='w-fit ease-in-out duration-300 rounded-3xl px-2 py-1 hover:text-black hover:bg-amber-50'>
+          <a href='#contact'>Contacto</a>
+        </li>
+      </ul>
+      <SocialNetworks />
+    </nav>
   );
 };
