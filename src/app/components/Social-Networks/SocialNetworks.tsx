360f808f8925be17de5cbb2c5edac3301e03f328
oiranca <samuelromeroarbelo@gmail.com>
2024-07-21 11:35:50 +0100

    fixed the social component and the lint style

--- src/app/components/Social-Networks/SocialNetworks.tsx
+++ src/app/components/Social-Networks/SocialNetworks.tsx
@@ -2,6 +2,7 @@ export const SocialNetworks = () => {
   const linkedin = './img/social/linkedin_logo.png';
   const x = '/img/social/X_logo.png';
   const github = '/img/social/github_logo.png';
+
   return (
     <section className='grid grid-cols-3 justify-items-center content-around gap-x-6 gap-y-2 p-2 w-fit '>
       <a
@@ -11,7 +12,7 @@ export const SocialNetworks = () => {
         rel='noopener noreferrer'
       >
         <img
-          className=' rounded-full w-8 h-8 md:w-12 md:h-12 hover:shadow-md hover:shadow-amber-50'
+          className='rounded-full w-8 h-8 md:w-12 md:h-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200'
           src={linkedin}
           alt='Link a mi perfil de LinkedIn'
         />
@@ -23,7 +24,7 @@ export const SocialNetworks = () => {
         rel='noopener noreferrer'
       >
         <img
-          className=' rounded-full w-8 h-8 md:w-12 md:h-12 hover:shadow-md hover:shadow-amber-50'
+          className='border border-amber-50 rounded-full w-8 h-8 md:w-12 md:h-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200'
           src={x}
           alt='Link a mi perfil de X anterior twitter'
         />
@@ -35,7 +36,7 @@ export const SocialNetworks = () => {
         rel='noopener noreferrer'
       >
         <img
-          className=' rounded-full w-8 h-8 md:w-12 md:h-12 hover:shadow-md hover:shadow-amber-50'
+          className=' rounded-full w-8 h-8 md:w-12 md:h-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200'
           src={github}
           alt='Link a mi perfil de GitHub'
         />
