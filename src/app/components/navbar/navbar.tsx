fafa2a22101880246a63d2bc0db1d2edd4f836a5
oiranca <samuelromeroarbelo@gmail.com>
2024-07-21 15:26:45 +0100

    wip: style for the about me component

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import { SocialNetworks } from './components/Social-Networks/SocialNetworks';
 export const Navbar = () => {
   return (
     <nav className='flex flex-col items-center'>
-      <ul className='w-full font-mono text-amber-50 grid grid-cols-2 grid-rows-2 justify-items-center content-around gap-x-4 gap-y-2 p-3 border border-gray-300 rounded-xl sm:text-xl md:text-2xl md:grid-cols-4 md:grid-rows-1 lg:grid-cols-4 lg:grid-rows-1 lg:text-3xl lg:w-fit'>
+      <ul className='w-full font-mono text-amber-50 grid grid-cols-2 grid-rows-2 justify-items-center content-around gap-x-4 gap-y-2 p-3 border border-gray-300 rounded-xl sm:text-xl md:text-2xl sm:grid-cols-4 sm:grid-rows-1 lg:grid-cols-4 lg:grid-rows-1 lg:text-3xl lg:w-fit'>
         <li className='w-fit transition-shadow ease-in-out duration-0 rounded-3xl px-2 py-1 hover:duration-500 hover:shadow-navShadow'>
           <a href='#about'>Sobre mí</a>
         </li>
